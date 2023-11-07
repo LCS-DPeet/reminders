1bd0ce56f3e6b816a2a2bc0d00e35f45343e3256
Danika Peet <DPeet@lcs.on.ca>
2023-11-07 15:14:18 -0500

    Initial Commit

---
+++ reminders/remindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  remindersApp.swift
+//  reminders
+//
+//  Created by Danika Peet on 2023-11-07.
+//
+
+import SwiftUI
+
+@main
+struct remindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
