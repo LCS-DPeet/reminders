365e49203c073ab8e39f686692266358d57312a1
Danika Peet <DPeet@lcs.on.ca>
2023-11-16 13:19:57 -0500

    I added the to do and fixed the top right symbols. I also fixed the date and the text of the first idem on the list

--- reminders/ContentView.swift
+++ reminders/ContentView.swift
@@ -9,13 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            
+            // the large to do before the list
+            HStack {
+                Text("To do")
+                    .font(.largeTitle)
+                    .fontDesign(.rounded)
+                    .bold()
+                    .foregroundStyle(.blue)
+                    .padding()
+            Spacer()
+            }
+                
+            //the list
+            List {
+                VStack {
+                    HStack {
+                        
+                        Image(systemName: "circle")
+                        
+                        Text("Call auto body shop")
+                    }
+                        
+                        Text("2023-11-16             ")
+                            .font(.footnote)
+                    
+                }
+                Text("a")
+                Text("a")
+                Text("a")
+                Text("a")
+            }
+            .listStyle(.plain)
+            
+            //top right symbols
+            
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Image(systemName: "chevron.backward")
+                        Text("List")
+                    }
+                    
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Image(systemName: "square.and.arrow.up")
+                        Image(systemName: "ellipsis.circle")
+                    }
+                    
+                }
+            }
+            
         }
-        .padding()
     }
 }
 
